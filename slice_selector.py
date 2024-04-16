085ff1b4b40d84a00b5c37fdb1bbca05f5d46ba4
Piyush Maiti <piyush.maiti@ucsf.edu>
2024-04-16 21:00:37 +0000

    Adding Freesurfer LUT

--- slice_selector.py
+++ slice_selector.py
@@ -28,114 +28,111 @@ Usage:
     ```
 """
 import numpy as np
+from freesurferlut import FreeSurferColorLUT
+
+lut_parser = FreeSurferColorLUT()
 
 freesurfer_lut = {
     "hippocampus": [17, 53],
-    "posterior cingulate": [1010, 1022, 1023, 1026, 2023, 3023, 4023],
     "cerebellum": [7, 8, 46, 47],
+    "superiorparietal": [1029, 2029],    
+    "cingulate": [1002, 1026, 1023, 1010, 2002, 2026, 2023, 2010],
 }
 
-def roi_mask(threed_image_array, roi):
+def fs_roi_mask(threed_image_array, roi=None, label_indices=None):
     """
     Generate a mask for the specified region of interest (ROI) based on FreeSurfer LUT.
+    Parameters
+    ----------
+    threed_image_array : np.ndarray
+        3D numpy array representing the aparc+aseg image from FreeSurfer
+    
+    roi : str
+        Region of interest (ROI) name.
+    
+    label_indices : list of int
+        List of label indices for the ROI.
 
-    Args:
-        threed_image_array (np.ndarray): 3D numpy array representing the image.
-        roi (str): Region of interest (ROI) name.
+    Returns
+    -------
+    mask : np.ndarray
+        3D numpy array representing the mask for the specified ROI containing 1s and 0s.
 
-    Returns:
-        np.ndarray: Mask representing the ROI.
+    Usage
+    -----
+    mask = fs_roi_mask(threed_image_array, roi='hippocampus')
+    mask = fs_roi_mask(threed_image_array, label_indices=[17, 53])
+
     """
+    mask = np.zeros_like(threed_image_array, dtype=np.uint8)
 
-    if roi in freesurfer_lut:
-        mask = np.zeros_like(threed_image_array, dtype=np.uint8)
+    if roi is not None and label_indices is not None:
+        raise ValueError("Only one of roi or label_indices should be specified.")
+    
+    elif roi is not None and label_indices is None:
         for value in freesurfer_lut[roi]:
             mask[threed_image_array == value] = 1
         return mask
+    
+    elif label_indices is not None and roi is None:
+        # Check if the label_indices are valid and present in the threed_image_array
+        if not all([value in threed_image_array for value in label_indices]):
+            raise ValueError("The specified ROI is not available in the provided aparc+aseg image.")
+        
+        else:
+            for value in label_indices:
+                mask[threed_image_array == value] = 1
+            return mask
+
     else:
-        raise ValueError("The specified ROI is not available in the FreeSurfer LUT.")
+        raise ValueError("The specified ROI is not available in the QC FreeSurfer LUT Dictionary.")
+    
+class SliceSelector:
+    def __init__(self, aparc):
+        self.aparc = aparc
 
+    def _select_slices(self, axis, roi=None, label_indices=None, percentile=None, num_slices=None):
+        if roi is not None and label_indices is not None:
+            raise ValueError("Only one of roi or label_indices should be specified.")
+        
+        elif roi is not None and label_indices is None:
+            mask = fs_roi_mask(self.aparc, roi=roi)
 
-def select_coronal_slice(threed_image_array, roi=None, percentile=None):
-    """
-    Select a coronal slice based on ROI: hippocampus and cerebellum.
-    1. Percentile form
-    2. Maximum area form
+        elif label_indices is not None and roi is None:
+            mask = fs_roi_mask(self.aparc, label_indices=label_indices)
 
-    Args:
-        threed_image_array (np.ndarray): 3D numpy array representing the image.
-        roi (str): Region of interest (ROI) name.
-        percentile (float): Percentile value for selecting the slice.
+        # Setting the axis to 0/sagittal/x or 1/coronal/y or 2/axial/z
+        axis_slices = np.where(np.any(mask, axis=tuple(set(range(3)) - {axis})))[0]
+        
+        if percentile is not None and num_slices is not None:
+            raise ValueError("Please specify either a percentile value or the number of slices, not both.")
+        
+        elif percentile is not None:
+            selected_slice = np.percentile(axis_slices, percentile*100)
+            return int(selected_slice)
+        
+        elif num_slices is not None:
+            selected_slice = np.percentile(axis_slices, 50)
+            slice_range_left = np.linspace(axis_slices[0], selected_slice, num_slices//2, dtype=int)
+            slice_range_right = np.linspace(selected_slice, axis_slices[-1], num_slices//2 + 1, dtype=int)[1:] 
+            slice_range = np.concatenate((slice_range_left, slice_range_right))
+            return [int(slice) for slice in slice_range]
+        
+        else:
+            raise ValueError("Please specify either a percentile value or the number of slices.")
 
-    Returns:
-        int: Index of the selected coronal slice.
-    """
-
-    if roi is None:
-        raise ValueError("Please specify a region of interest (ROI) for selecting the coronal slice.")
-
-    mask = roi_mask(threed_image_array, roi)
-
-    if percentile is not None:
-        print(f"Selecting {percentile*100}th percentile slice for {roi}...")
-        coronal_indices = np.where(np.any(mask, axis=(0, 2)))[0]
-        selected_slice = np.percentile(coronal_indices, percentile*100)
-    else:
-        print(f"Selecting the coronal slice with the maximum area for {roi}...")
-        mask_sum = np.sum(mask, axis=(0, 2))
-        selected_slice = np.argmax(mask_sum)
-
-    return int(selected_slice)
-
-def select_two_coronal_slices(threed_image_array, percentile=None):
-    """
-    Select two coronal slices based on ROI: hippocampus and cerebellum.
-    1. Percentile form
-    2. Maximum area form
-
-    Args:
-        threed_image_array (np.ndarray): 3D numpy array representing the image.
-        percentile (float): Percentile value for selecting the slices.
-
-    Returns:
-        tuple: Indices of the selected coronal slices.
-    """
-    if percentile is not None:
-        print(f"Selecting the coronal slices with {percentile*100}th percentile for hippocampus and cerebellum...")
-        hippocampus_slice = select_coronal_slice(threed_image_array, roi="hippocampus", percentile=percentile)
-        cerebellum_slice = select_coronal_slice(threed_image_array, roi="cerebellum", percentile=percentile)
-    else:
-        print(f"Selecting the coronal slices with the maximum area for hippocampus and cerebellum...")
-        hippocampus_slice = select_coronal_slice(threed_image_array, roi="hippocampus")
-        cerebellum_slice = select_coronal_slice(threed_image_array, roi="cerebellum")
-
-    return tuple([cerebellum_slice, hippocampus_slice])
-
-def select_sagittal_slice(threed_image_array, roi=None, percentile=None):
-    """
-    Select the sagittal slice with the maximum area for the specified region of interest (ROI).
-
-    Args:
-        threed_image_array (np.ndarray): 3D numpy array representing the image.
-        roi (str): Region of interest (ROI) name.
-        percentile (float): Percentile value for selecting the slice.
-
-    Returns:
-        int: Index of the selected sagittal slice.
-    """
-
-    if roi is None:
-        raise ValueError("Please specify a region of interest (ROI) for selecting the sagittal slice.")
-
-    mask = roi_mask(threed_image_array, roi)
-
-    if percentile is not None:
-        print(f"Selecting {percentile*100}th percentile slice for {roi}...")
-        sagittal_indices = np.where(np.any(mask, axis=(0, 1)))[0]
-        selected_slice = np.percentile(sagittal_indices, percentile*100)
-    else:
-        print(f"Selecting the sagittal slice with the maximum area for {roi}...")
-        mask_sum = np.sum(mask, axis=(0, 1))
-        selected_slice = np.argmax(mask_sum)
-
-    return int(selected_slice)
+    def select_slices(self, axis, roi=None, label_indices=None, percentile=None, num_slices=None):
+        if isinstance(axis, str):
+            _axis = axis.lower()[0]
+            if _axis in ("x", "s"):
+                print("Selecting sagittal slice/s")
+                axis = 0
+            elif _axis in ("y", "c"):
+                print("Selecting coronal slice/s")
+                axis = 1
+            elif _axis in ("z", "a"):
+                print("Selecting axial slice/s")
+                axis = 2
+            else:
+                raise ValueError(f"axis {axis} not recognized")
+        return self._select_slices(axis, roi, label_indices, percentile, num_slices)
