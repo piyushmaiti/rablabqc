9dc8d6d440558e2e5739ec7be883c16fd8642693
piyushmaiti <piyush.maiti@ucsf.edu>
2024-07-02 17:04:18 +0000

    vmax set to 140;changed aparc+aseg segmentation color for subcortical gray matter and cerebellar gray matter

--- mri_slices.py
+++ mri_slices.py
@@ -58,7 +58,7 @@ cmap_yellow2 = LinearSegmentedColormap.from_list('custom_color', [(249/255, 195/
 cmap_yellow2.set_under(alpha=0)  # Set transparency for zeros
 #  ____________________________________________________________ IMAGE THRESHOLDS _____________________________________________________________ #
 
-mri_vmax = 120
+mri_vmax = 140
 
 # ______________________________________________________________ TEMPLATE SLICES  ____________________________________________________________ #
 
@@ -76,9 +76,9 @@ def load_tpm(path, orientation="LAS"):
 
 tpm_image = load_tpm(tpm_file)[:,:,:,0]
 
-template_axial_slices = [15,23, 34, 45, 56, 67, 78]
-template_sagittal_slices = [55,44]
-template_coronal_slices = [48,70]
+template_axial_slices = [14, 25, 36, 47, 58, 69, 80, 91]
+template_sagittal_slices = [44, 55, 77]
+template_coronal_slices = [48, 70, 83]
 
 #  _____________________________________________________________ DEFINE FUNCTIONS _____________________________________________________________ #
 
@@ -140,7 +140,6 @@ class MRIQCplots:
         with open(mask_reslice_matlab_script, 'r') as template_file:
             script_content = template_file.read()
 
-        # Replace placeholders with actual paths
         script_content = script_content.replace('<RTPM_PATH>', rtpm_path)
         script_content = script_content.replace('<DATA_PATH>', path)
         
@@ -187,7 +186,7 @@ class MRIQCplots:
             print("Output:\n", matprocess.stdout)
 
         else:
-            print("Resliced image already exists for ", id, ". Loading the resliced image...")
+            print("Resliced image exists for ", id, ". Loading the resliced image...")
             
         img = nib.load(resliced_image_path)
         img_ornt = io_orientation(img.affine)
@@ -270,7 +269,7 @@ class MRIQCplots:
         numpy.ndarray
             The image data as a numpy array.
         """
-        self.basename = self.nu_img.split('/')[-1].split('_')[0]+'_'+self.nu_img.split('/')[-1].split('_')[1]+'_'+self.nu_img.split('/')[-1].split('_')[2]
+        self.basename = os.path.basename(self.nu_img)
         self.nu_img_filename = os.path.basename(self.nu_img)
         self.nu_img = self.load_nii_resliced(self.nu_img)
         
@@ -326,7 +325,7 @@ class MRIQCplots:
 
     def aparc_img_slices(self):
         """
-        1. This function generates the aparc_img slices with the cortical labels thresholded between 1000 and 4000 to be overlaid on the nu_img.
+        1. This function generates the aparc_img slices with the cortical labels thresholded between 1000 and 3000 to be overlaid on the nu_img.
         2. The function uses nu_img as the underlay image as a base to determine the bounding box for the slices so that it is accurately cropped.
         For more information about the cortical labels(and the threshold values), refer to the following link: https://surfer.nmr.mgh.harvard.edu/fswiki/FsTutorial/AnatomicalROI/FreeSurferColorLUT
         
@@ -340,7 +339,7 @@ class MRIQCplots:
             select_axial_slices=self.axial_slices,
             select_sagittal_slices=self.sagittal_slices,
             select_coronal_slices=self.coronal_slices,
-            mask_lower_threshold=1000, mask_upper_threshold=4000,
+            mask_lower_threshold=1000, mask_upper_threshold=3000,
             crop_neck=self.aparc_img if self.crop_neck else None).generate_qc_images()
     
         return aparc_img_slices
@@ -379,13 +378,44 @@ class MRIQCplots:
             "left_putamen": (12, 13),
             "right_putamen": (51, 52),
             "left_caudate": (11, 12),
-            "right_caudate": (50, 51)
+            "right_caudate": (50, 51),
+            #"left_cerebellum_cortex": (8, 9),
+            #"right_cerebellum_cortex": (47, 48),
         }
 
         # Generate subcortical images
         return {key: generate_subcortical(*threshold) for key, threshold in threshold_ranges.items()}
 
+    def generate_cblgm_slices(self):
+        """
+        This function generates the subcortical regions from the aparc_img. This uses the nu_img as the underlay image to determine the bounding box for the slices so that it is accurately cropped.
+        For more information regarding the subcortical regions(and the thresholds applied to get the regions), refer to the following link: https://surfer.nmr.mgh.harvard.edu/fswiki/FsTutorial/AnatomicalROI/FreeSurferColorLUT
+        Returns
+        -------
+        numpy.ndarray : The slices with the subcortical regions.
+        """
+        
+        def generate_cblgm(lower_threshold, upper_threshold):
+            _, image = QCImageGenerator(
+                underlay_img = self.nu_img,
+                overlay_img = self.aparc_img,
+                select_axial_slices = self.axial_slices,
+                select_sagittal_slices = self.sagittal_slices,
+                select_coronal_slices = self.coronal_slices,
+                mask_lower_threshold = lower_threshold,
+                mask_upper_threshold = upper_threshold,
+                crop_neck = self.aparc_img if self.crop_neck else None).generate_qc_images()
+            return image
+        
+        # Define threshold ranges for subcortical regions
+        threshold_ranges = {
+            "left_cerebellum_cortex": (8, 9),
+            "right_cerebellum_cortex": (47, 48)
+        }
 
+        # Generate subcortical images
+        return {key: generate_cblgm(*threshold) for key, threshold in threshold_ranges.items()}
+    
     def c1_image_slices(self):
         """
         1. This function generates the slices for the c1(spm segmentation of the Tissue Probability Maps) i.e. the Grey matter (containing high densities of unmyelinated neurons).
@@ -451,33 +481,42 @@ class MRIQCplots:
         sns.heatmap(self.nu_img_slices(), cmap='gray', vmax=mri_vmax, cbar=False, ax=axes) 
         # Plotting the lines representing the slices
         sns.heatmap(self.nu_img_lines(), cmap=cmap_yellow2, vmin=0.5, cbar=False, ax=axes)
-        axes.text(20, 30, 'L', fontsize=15, color='white')
-        axes.text(150, 30, 'R', fontsize=15, color='white')
-        axes.set_title(f"{self.nu_img_filename}", fontsize=16, color='white', loc='left')
+        axes.text(10, 30, 'L', fontsize=10, color='white')
+        axes.text(150, 30, 'R', fontsize=10, color='white')
+        axes.set_title(f"{self.nu_img_filename}", fontsize=10, color='white', loc='left')
         axes.axis('off')
+        axes.set_aspect('equal')  # Set aspect ratio to be equal
 
     def plot_nu_img_aparc_slices(self, axes):
         """
         This function the aparc+aseg and the subcortical regions overlaid on the nu_img slices.
         """
-        sns.heatmap(self.nu_img_slices(), cmap='gray', vmax=mri_vmax, cbar=False, ax=axes[0])
+        sns.heatmap(self.nu_img_slices(), cmap='gray', vmin=0, vmax=mri_vmax, cbar=False, ax=axes[0])
         sns.heatmap(self.nu_img_lines(), cmap=cmap_yellow2, vmin=0.5, cbar=False, ax=axes[0])
-        axes[0].text(20, 30, 'L', fontsize=15, color='white')
-        axes[0].text(150, 30, 'R', fontsize=15, color='white')
-        axes[0].set_title(f"{self.nu_img_filename}", fontsize=16, color='white', loc='left')
+        axes[0].text(10, 30, 'L', fontsize=10, color='white')
+        axes[0].text(150, 30, 'R', fontsize=10, color='white')
+        axes[0].set_title(f"{self.nu_img_filename}", fontsize=10, color='white', loc='left')
         axes[0].axis('off')
+        axes[0].set_aspect('equal') # Set aspect ratio to be equal
 
         # nu MRI image 
-        sns.heatmap(self.nu_img_slices(), cmap='gray', vmax=mri_vmax, cbar=False, ax=axes[1])
+        sns.heatmap(self.nu_img_slices(), cmap='gray', vmin=0, vmax=mri_vmax, cbar=False, ax=axes[1])
         # Adding the aparc+aseg slices
-        sns.heatmap(self.aparc_img_slices(), cmap=cmap_red, vmin=0.1, cbar=False, ax=axes[1])
+        sns.heatmap(self.aparc_img_slices(), cmap=cmap_red, vmax=1, mask=self.aparc_img_slices()==0, cbar=False, ax=axes[1])
         # Adding the subcortical regions
         subcortical_regions = self.generate_subcortical_slices()
+        subcortical_regions_cmap = (['#3EBCD2'])
         for key, value in subcortical_regions.items():
-            sns.heatmap(value, cmap=cmap_red, vmin=0.1, cbar=False, ax=axes[1])
+            sns.heatmap(value, cmap=subcortical_regions_cmap, vmin=0.1, mask = value == 0, cbar=False, ax=axes[1])
+        
+        cblgm_regions = self.generate_cblgm_slices()
+        cblgm_regions_cmap = (['#2E45B8'])
+        for key, value in cblgm_regions.items():
+            sns.heatmap(value, cmap=cblgm_regions_cmap, vmin=0.1, mask = value == 0, cbar=False, ax=axes[1])
 
-        axes[1].set_title(f"Overlay: {self.aparc_img_filename}", fontsize=16, color='white', loc='left')
+        axes[1].set_title(f"Overlay: {self.aparc_img_filename}", fontsize=10, color='white', loc='left')
         axes[1].axis('off')
+        axes[1].set_aspect('equal')  # Set aspect ratio to be equal
 
     def plot_c1_img_slices(self, axes):
         """
@@ -485,29 +524,33 @@ class MRIQCplots:
         overlaid on the nu_img slices.
         """
         # Plotting the nu_img slices
-        sns.heatmap(self.nu_img_slices(), cmap='gray', vmax = mri_vmax, cbar=False, ax=axes)
+        sns.heatmap(self.nu_img_slices(), cmap='gray', vmin=0, vmax=mri_vmax, cbar=False, ax=axes)
         # Plotting the c1 slices
-        sns.heatmap(self.c1_image_slices(), cmap=cmap_red, vmin = 0.1, cbar=False, ax=axes)
-        axes.set_title(f"Overlay: {self.c1_img_filename} (voxels > 0.8)", fontsize=16, color='white', loc='left')
+        sns.heatmap(self.c1_image_slices(), cmap=cmap_red, vmax=1, mask=self.c1_image_slices()==0, cbar=False, ax=axes)
+        axes.set_title(f"Overlay: {self.c1_img_filename} (voxels > 0.8)", fontsize=10, color='white', loc='left')
         axes.axis('off')
+        axes.set_aspect('equal')  # Set aspect ratio to be equal
 
     def plot_affine_nu_img_slices(self, axes):
         affine_nu_image_slices, tpm_img_slices = self.affine_nu_img_slices()
 
-        sns.heatmap(affine_nu_image_slices, cmap='gray', vmax = mri_vmax, cbar=False, ax=axes)
-        sns.heatmap(tpm_img_slices, cmap=cmap_pink, vmin = 0.1, alpha = 0.2, cbar=False, ax=axes)
-        sns.heatmap(ImageProcessor.contour_image(tpm_img_slices), cmap = cmap_pink, vmin = 0.1, cbar = False, ax=axes)
-        axes.set_title(f"Underlay: {self.affine_nu_img_filename}\nOverlay: TPM.nii (c1, voxels > 0.3)", fontsize=16, color='white', loc='left')
+        sns.heatmap(affine_nu_image_slices, cmap='gray', vmin=0, vmax=mri_vmax, cbar=False, ax=axes)
+        sns.heatmap(tpm_img_slices, cmap=cmap_pink, vmax=1, mask=tpm_img_slices==0, alpha = 0.2, cbar=False, ax=axes)
+        contour_arr = ImageProcessor.contour_image(tpm_img_slices)
+        sns.heatmap(contour_arr, cmap = cmap_pink, vmax=1, mask=contour_arr==0, cbar = False, ax=axes)
+        axes.set_title(f"Underlay: {self.affine_nu_img_filename}\nOverlay: TPM.nii (c1, voxels > 0.3)", fontsize=10, color='white', loc='left')
         axes.axis('off')
+        axes.set_aspect('equal')  # Set aspect ratio to be equal
 
     def plot_warped_nu_img_slices(self, axes):
         warped_nu_image_slices, wnu_tpm_img_slices = self.warped_nu_img_slices()
-        sns.heatmap(warped_nu_image_slices, cmap='gray', vmax = mri_vmax, cbar=False, ax=axes)
-        sns.heatmap(wnu_tpm_img_slices, cmap=cmap_pink, vmin = 0.1, alpha = 0.2, cbar=False, ax=axes)
-        sns.heatmap(ImageProcessor.contour_image(wnu_tpm_img_slices), cmap = cmap_pink, vmin = 0.1, cbar = False, ax=axes)
-        axes.set_title(f"Underlay: {self.warped_nu_img_filename}\nOverlay: TPM.nii (c1, voxels > 0.3)", fontsize=16, color='white', loc='left')
+        sns.heatmap(warped_nu_image_slices, cmap='gray', vmin=0, vmax=mri_vmax, cbar=False, ax=axes)
+        sns.heatmap(wnu_tpm_img_slices, cmap=cmap_pink, vmax=1, mask=wnu_tpm_img_slices==0, alpha = 0.2, cbar=False, ax=axes)
+        contour_arr = ImageProcessor.contour_image(wnu_tpm_img_slices)
+        sns.heatmap(contour_arr, cmap = cmap_pink, vmax=1, mask=contour_arr==0, cbar = False, ax=axes)
+        axes.set_title(f"Underlay: {self.warped_nu_img_filename}\nOverlay: TPM.nii (c1, voxels > 0.3)", fontsize=10, color='white', loc='left')
         axes.axis('off')
-
+        axes.set_aspect('equal')  # Set aspect ratio to be equal
     #  _____________________________________________________________ Plotting the slices _____________________________________________________________ #
 
     def plot_slices(self,output_path):
@@ -529,28 +572,28 @@ class MRIQCplots:
         plt.figure(facecolor='black')
 
         # Only nu_img is provided
-        if self.nu_img is not None and self.aparc_img is None and self.c1_img is None and self.affine_nu_img is None and self.warped_nu_img is None:            
-            fig, axes = plt.subplots(1, 1, figsize=(26, 2.5))
+        if self.nu_img is not None and self.aparc_img is None and self.c1_img is None and self.affine_nu_img is None and self.warped_nu_img is None:
             
+            fig, axes = plt.subplots(1, 1, figsize= (30, 2))
             self.plot_mri_slices(axes)
 
         # nu_img and aparc_img are provided
         elif self.aparc_img is not None and self.c1_img is None and self.affine_nu_img is None and self.warped_nu_img is None:
-            fig, axes = plt.subplots(2, 1, figsize=(26, 6))
+            fig, axes = plt.subplots(2, 1, figsize=(12, 2))
 
             self.plot_nu_img_aparc_slices(axes)
 
 
         # nu_img, aparc_img, and c1_img are provided
         elif self.aparc_img is not None and self.c1_img is not None and self.affine_nu_img is None and self.warped_nu_img is None:            
-            fig, axes = plt.subplots(3, 1, figsize=(26, 9))
+            fig, axes = plt.subplots(3, 1, figsize=(12, 4))
             
             self.plot_nu_img_aparc_slices(axes)
             self.plot_c1_img_slices(axes[2])
         
         # If nu_img, aparc_img, c1_img, and affine_nu_img are provided
         elif self.aparc_img is not None and self.c1_img is not None and self.affine_nu_img is not None and self.warped_nu_img is None:            
-            fig, axes = plt.subplots(4, 1, figsize=(25, 12))
+            fig, axes = plt.subplots(5, 1, figsize=(12, 6))
             
             self.plot_nu_img_aparc_slices(axes)
             self.plot_c1_img_slices(axes[2])
@@ -558,15 +601,18 @@ class MRIQCplots:
 
         # If nu_img, aparc_img, c1_img, affine_nu_img, and warped_nu_img are provided
         elif self.aparc_img is not None and self.c1_img is not None and self.affine_nu_img is not None and self.warped_nu_img is not None:            
-            fig, axes = plt.subplots(5, 1, figsize=(26, 16))
-            
+            fig, axes = plt.subplots(5, 1, figsize=(12, 8))
             self.plot_nu_img_aparc_slices(axes)
             self.plot_c1_img_slices(axes[2])
             self.plot_affine_nu_img_slices(axes[3])
             self.plot_warped_nu_img_slices(axes[4])
 
-
         fig.patch.set_facecolor('black')
-        plt.subplots_adjust(top=1, wspace=0, hspace=0.3)
-       
-        plt.savefig(os.path.join(output_path, self.basename + '.png'), facecolor='black', bbox_inches='tight', dpi=500)
+        plt.tight_layout()
+        #plt.subplots_adjust(hspace=0.5)
+
+        # Remove the extension from the basename to save the image
+        file_name = os.path.splitext(self.basename)[0]
+        # Removing the '_nu' from the filename and adding the '_qc' to the filename
+        file_name = file_name.replace('_nu', '_qc')
+        plt.savefig(os.path.join(output_path, file_name + '.png'), facecolor='black', bbox_inches='tight', dpi=500)
